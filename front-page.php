2538359d8fc16732e1eab81e3241e1115b844498
Martin Martev <mmartev@gmail.com>
2017-04-19 17:12:32 +0300

    finalize front page

--- front-page.php
+++ front-page.php
@@ -2,9 +2,6 @@
 /**
  * The front page template file
  *
- * If the user has selected a static page for their homepage, this is what will
- * appear.
- * Learn more: https://codex.wordpress.org/Template_Hierarchy
  *
  * @package WordPress
  * @subpackage noframe
@@ -13,21 +10,23 @@
  */
 get_header(); ?>
 <div class="front-page--container">
-  <div class="front-page--left mdl-color--blue-grey-300 mdl-color-text--white">
-    <h1>NO FRAME<br />PHOTOGRAPHY</h1>
-    <div class="front-page--left-menu">
-      <p class="">Семейна фотография</p>
-      <p class="">Бизнес</p>
-      <p class="">Лични проекти</p>
-    </div>
-    <div class="front-page--left-action">
-      <h1 class="mdc-typography--title">Научете повече</h1>
-    </div>
-  </div>
-  <div class="front-page--right">
-    <div class="slider--container">
+  <div class="front-page--left">
+    <div class="slider--container mdl-color--blue-100">
       <div class="slide slider--slide-1"></div>
       <div class="slide slider--slide-2"></div>
+    </div>
+  </div>
+  <div class="front-page--right mdl-color--grey-200">
+    <div class="front-page--right-inner">
+      <h1 class="front-page--right-title">NOFRAME<br />PHOTOGRAPHY</h1>
+      <div class="front-page--right-message mdl-color-text--grey-500">
+        <p>Най-доброто място за семейна фотография,<br />фирмени снимки и лични проекти!</p>
+      </div>
+      <div class="front-page--right-action">
+        <a href="#">
+          <h1 class="front-page--right-button mdc-typography--title mdl-color-text--white">Научете повече</h1>
+        </a>
+      </div>
     </div>
   </div>
 </div>
--- functions.php
+++ functions.php
@@ -12,6 +12,7 @@
 function noframe_scripts() {
  	// Add custom fonts, used in the main stylesheet.
  	wp_enqueue_style( 'noframe-fonts', 'https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en');
+ 	wp_enqueue_style( 'noframe-fonts', 'https://fonts.googleapis.com/css?family=Nunito+Sans:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en');
  	// Add custom icons, used in the main stylesheet.
  	wp_enqueue_style( 'noframe-icons', 'https://fonts.googleapis.com/icon?family=Material+Icons');
  	// Theme stylesheet.
