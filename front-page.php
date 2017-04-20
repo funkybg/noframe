1f0a101c1574d1c09d11053adffac7e928b7ae19
Martin Martev <mmartev@gmail.com>
2017-04-20 11:05:03 +0300

    add category links to the landing page

--- front-page.php
+++ front-page.php
@@ -19,12 +19,26 @@ get_header(); ?>
   <div class="front-page--right mdl-color--grey-200">
     <div class="front-page--right-inner">
       <h1 class="front-page--right-title">NOFRAME<br />PHOTOGRAPHY</h1>
-      <div class="front-page--right-message mdl-color-text--grey-500">
+      <div class="front-page--right-message mdl-color-text--blue-grey-400">
         <p>Най-доброто място за семейна фотография,<br />фирмени снимки и лични проекти!</p>
       </div>
       <div class="front-page--right-action">
-        <a href="#">
+        <a href="/sample-page/">
           <h1 class="front-page--right-button mdc-typography--title mdl-color-text--white">Научете повече</h1>
+        </a>
+      </div>
+      <div class="front-page--right-subaction">
+        <span class="mdl-color-text--blue-grey-400">Или изберете категория:<br /></span>
+        <a href="/family/">
+          <span class="front-page--right-subnav">Семейна фотография</span>
+        </a>
+        <span class="mdl-color-text--blue-grey-400"> / </span>
+        <a href="/business/">
+          <span class="front-page--right-subnav">Фирмени снимки</span>
+        </a>
+        <span class="mdl-color-text--blue-grey-400"> / </span>
+        <a href="/personal/">
+          <span class="front-page--right-subnav">Лични проекти</span>
         </a>
       </div>
     </div>
